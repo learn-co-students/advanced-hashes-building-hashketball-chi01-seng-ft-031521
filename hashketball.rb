f649609003b8f7d7ba789783168fa36552743234
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-03-02 16:32:14 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,8 @@
 def game_hash
   {
     home: {
-      team_name: "",
-      colors: [],
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
       players: [ 
         {
           player_name: "Alan Anderson", 
@@ -63,8 +63,8 @@ def game_hash
       ]
     },
     away: {
-      team_name: "",
-      colors: [],
+      team_name: "Charlotte Hornets",
+      colors: ["Turquioise", "Purple"],
       players: [
           {
           player_name: "Jeff Adrien", 
